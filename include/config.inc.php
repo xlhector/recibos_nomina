318dc68ee7289d4bb478d29e18071beadff1d11d
Hector dorador <hector.dor.ar@gmail.com>
2018-01-01 18:47:32 -0700

    inicio repo

---
+++ include/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+	define("_url_pac", "https://cfdiws.sedeb2b.com/EdiwinWS/services/CFDi?wsdl");
+	define("_usuario_pac", "ZAHM850924656");
+	define("_password_pac", "xglvkhxqi");
+
+	define("_database_driver", "mysql");
+  	define("_database_host", "127.0.0.1");
+	define("_database_user", "root");
+	define("_database_password", 'zocross54');
+	define("_database_name", "recibos_nomina");
+
+
+    //define("_user_database", "lenin_facturate");
+	//define("_password_database", "4b498406");
+?>
